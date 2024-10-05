110fc6d63c3974d31c35b3f5907bbed20906b5ac
wfsbear <47589206+ilikecinepol@users.noreply.github.com>
2024-10-05 23:47:29 +0300

    Update marine_server.py

--- marine_server.py
+++ marine_server.py
@@ -1,7 +1,7 @@
 import serial
 from flask import Flask, render_template, jsonify, request
 
-# Настройка последовательного порта (замени '/dev/ttyUSB0' на свой порт)
+# Настройка последовательного порта
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 
 app = Flask(__name__)
@@ -10,33 +10,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/control', methods=['POST'])
-def control():
-    data = request.json
-    pump1_speed = data.get('pump1_speed')
-    pump2_speed = data.get('pump2_speed')
-
-    if pump1_speed is not None:
-        command = f'P1:{pump1_speed}\n'
-        ser.write(command.encode())
-
-    if pump2_speed is not None:
-        command = f'P2:{pump2_speed}\n'
-        ser.write(command.encode())
-
-    return jsonify({'status': 'OK'})
-
 @app.route('/gyro')
 def gyro():
     if ser.in_waiting > 0:
         try:
             gyro_data = ser.readline().decode('utf-8').strip()
-            gx, gy, gz = map(float, gyro_data.split(','))  # Разделение данных
-            return jsonify({'gx': gx, 'gy': gy, 'gz': gz})
+            gx, gy = map(float, gyro_data.split(','))  # Разделение данных по запятой
+            return jsonify({'gx': gx, 'gy': gy})
         except Exception as e:
             print(f"Ошибка чтения данных: {e}")
-            return jsonify({'gx': 'N/A', 'gy': 'N/A', 'gz': 'N/A'})
-    return jsonify({'gx': 'N/A', 'gy': 'N/A', 'gz': 'N/A'})
+            return jsonify({'gx': 'N/A', 'gy': 'N/A'})
+    return jsonify({'gx': 'N/A', 'gy': 'N/A'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
